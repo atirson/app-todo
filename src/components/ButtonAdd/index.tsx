e1d215c562371efe2f34b3de1fb25f1398ad1a0e
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-01-16 00:19:23 -0300

    refactor: Added function to add new todo

--- src/components/ButtonAdd/index.tsx
+++ src/components/ButtonAdd/index.tsx
@@ -1,9 +1,14 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { styles } from './styles';
 
-export function ButtonAdd() {
+
+interface ButtonAddProps {
+  onAdd: (todo: any) => void;
+}
+
+export function ButtonAdd({ onAdd }: ButtonAddProps) {
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={onAdd}>
       <Image source={require('../../../assets/plus.png')} />
     </TouchableOpacity>
   );
