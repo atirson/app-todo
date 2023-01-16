8b135dec393ec421540f82ceaf16f6519a6451d6
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-01-16 00:17:37 -0300

    refactor: new atributes to be dinamic component

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -1,17 +1,34 @@
+import { useMemo, useState } from 'react';
 import { View, Text } from 'react-native'
+import { Todo } from '../Card';
 
 import { styles } from './styles'
 
-export function Progress() {
+interface ProgressProps {
+  items: Todo[]
+}
+
+export function Progress({ items }: ProgressProps) {
+  const [created, setCreated] = useState(0)
+  const [concluded, setConcluded] = useState(0)
+
+  useMemo(() => {
+    const created = items.filter(item => item.status === 'pending').length
+    const concluded = items.filter(item => item.status === 'done').length
+
+    setCreated(created)
+    setConcluded(concluded)
+  }, [items])
+
   return (
     <View style={styles.container}>
       <View style={styles.task}>
         <Text style={styles.taskText}>Criadas</Text>
-        <Text style={styles.number}>0</Text>
+        <Text style={styles.number}>{created}</Text>
       </View>
       <View style={styles.concluded}>
         <Text style={styles.concludedText}>Concluídas</Text>
-        <Text style={styles.number}>1</Text>
+        <Text style={styles.number}>{concluded}</Text>
       </View>
     </View>
   );
